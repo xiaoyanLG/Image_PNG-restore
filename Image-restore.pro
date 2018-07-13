e06f30ab6eccd529079f48ed1467fd2890410c06
luogang <luogang@acoinfo.com>
2018-07-13 09:56:37 +0800

    First init!

---
+++ Image-restore.pro
@@ -0,0 +1,20 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2018-06-12T14:39:17
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = Image-restore
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
---
+++ mainwindow.cpp
@@ -0,0 +1,54 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QFileDialog>
+#include <QDir>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_pushButton_2_clicked() // 选择源文件路径
+{
+    ui->lineEdit->setText(QFileDialog::getExistingDirectory(this));
+}
+
+void MainWindow::on_pushButton_3_clicked() // 选择保存路径
+{
+    ui->lineEdit_2->setText(QFileDialog::getExistingDirectory(this));
+}
+
+void MainWindow::on_pushButton_clicked() // 修复
+{
+    QDir dir(ui->lineEdit->text());
+    QFileInfoList list = dir.entryInfoList(QStringList() << "*.png");
+    for (int i = 0; i < list.size(); ++i) {
+        QFileInfo fileInfo = list.at(i);
+        QImage image(fileInfo.filePath());
+        if (image.isNull())
+        {
+            continue;
+        }
+
+        if (ui->lineEdit_2->text().isEmpty())
+        {
+            image.save(fileInfo.filePath());
+        }
+        else
+        {
+            image.save(ui->lineEdit_2->text() + QDir::separator() + fileInfo.fileName());
+        }
+
+        ui->statusBar->showMessage(fileInfo.filePath());
+    }
+
+    QMessageBox::information(this, QStringLiteral("完成"), QStringLiteral("全部修复完成"));
+}
